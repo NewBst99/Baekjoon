d6d7f59127a95f3ae24472633b9d7c2dd6ce9b42
mbjin <90257022+NewBst99@users.noreply.github.com>
2023-07-24 22:12:35 +0900

    Add files via upload

---
+++ Baek24262.java
@@ -0,0 +1,13 @@
+package testbaek;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Baek24262 {
+    public static void main(String[] args) {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+       System.out.println("1");
+       System.out.println("0");
+    }
+}
