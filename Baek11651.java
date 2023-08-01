478da7c1c9dd5447e1f184a950a5d74ffc3d0426
mbjin <90257022+NewBst99@users.noreply.github.com>
2023-08-01 23:28:48 +0900

    check

---
+++ Baek11651.java
@@ -0,0 +1,38 @@
+package testbaek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Baek11651 {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	int num = Integer.parseInt(br.readLine());
+    	int[][] ary = new int[num][2];
+    	
+    	
+    	for(int i = 0; i < num; i++) {
+    		String[] temp = br.readLine().split(" ");
+    		ary[i][0] = Integer.parseInt(temp[0]);
+    		ary[i][1] = Integer.parseInt(temp[1]);
+    	}
+
+    	Arrays.sort(ary, new Comparator<int[]>() {
+    		@Override
+    		public int compare(int[] num1, int[] num2) {
+    			if(num1[1] == num2[1]) {
+    				return Integer.compare(num1[0], num2[0]);
+    			} else {
+    				return Integer.compare(num1[1], num2[1]);
+    			}
+    		}
+    	});
+
+    	for(int i=0; i<num; i++) {
+        	System.out.println(ary[i][0] +" "+ ary[i][1]);
+    	}
+    }  
+}
